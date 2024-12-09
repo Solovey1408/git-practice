deb8964102525b17edff82b073ae06197be3d7ed
Solovey1408 <songfromredstone@gmail.com>
2024-12-09 13:20:08 +0300

    ДЗ Выполнение простейших операций

---
+++ main.py
@@ -0,0 +1,36 @@
+a = 5
+b = 3
+
+sum = a + b
+difference = a - b
+
+print(sum, difference)
+
+x = 10
+y = 2
+
+product = x * y
+quotient = x / y
+
+print(product, quotient)
+
+m = 8
+n = 7
+
+integer_division = m // n
+remainder_of_division = m % n
+
+print(integer_division, remainder_of_division)
+
+base = 2
+exponent = 2
+
+raise_to_the_power = base ** exponent
+
+print(raise_to_the_power)
+
+result1 = (a + b) * y
+result2 = (x / b) + n
+result3 = (m ** y) - a
+
+print(result1, result2, result3)
