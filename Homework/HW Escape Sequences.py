aa66866a6418395db4350ad673b76787a1ba9214
Solovey1408 <songfromredstone@gmail.com>
2024-12-16 12:17:13 +0300

    ДЗ: Использование экранирующих последовательностей в Python

---
+++ Homework/HW Escape Sequences.py
@@ -0,0 +1,19 @@
+double_quote_example = "She said, \"Hello!\""
+print(double_quote_example)
+
+single_quote_example = 'It\'s a nice day!'
+print(single_quote_example)
+
+backslash_example = "Path to the file is C:\\Пользователи\\songf\\Desktop\\EduIT\\BasicPython\\1\\ILYA_SOLOVYEV\\Homework"
+print(backslash_example)
+
+new_line_example = "Hello,\nWorld!"
+print(new_line_example)
+
+tab_example = "Item1\tItem2\tItem3"
+print(tab_example)
+
+multi_line_example = """This is a "multi-line" string
+that spans multiple lines
+and includes special characters like\ttabs and\nnewlines."""
+print(multi_line_example)
