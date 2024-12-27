dafd1b523e14b20b09aebf306b95a4292be1b4ef
Solovey1408 <songfromredstone@gmail.com>
2024-12-27 17:27:48 +0300

    ДЗ: Переводчик чисел в текст

---
+++ Homework/HW Translator of numbers into text.py
@@ -0,0 +1,14 @@
+question_1 = int(input('Введите число от 1 до 5: '))
+
+if question_1 == 1:
+    print('One')
+elif question_1 == 2:
+    print('Two')
+elif question_1 == 3:
+    print('Three')
+elif question_1 == 4:
+    print('Four')
+elif question_1 == 5:
+    print('Five')
+else:
+    print('Неверное число')
