14bbdf4e3047a951a7bb20af4db1925b6bef40e1
Solovey1408 <songfromredstone@gmail.com>
2025-01-11 16:53:40 +0300

    ДЗ: Замена элементов списка

---
+++ Homework/HW Replacing list items.py
@@ -0,0 +1,10 @@
+  # Напишите программу, которая создает список из 5 строк
+  # и меняет первый и последний элементы в нём.
+user_list = ["one", "two", "three", "four", "five"]
+
+print(user_list)
+
+user_list[0] = "1"
+user_list[-1] = "5"
+
+print(user_list)
