92a372ecb77331239856d98332ec21abadafc00c
Solovey1408 <songfromredstone@gmail.com>
2025-01-20 12:58:09 +0300

    ДЗ: Проверка пароля

--- HOMEWORK Try #2/HW №22 Password verification.py
+++ HOMEWORK Try #2/HW №22 Password verification.py
@@ -1,11 +1,12 @@
-user_password = '123qwerty456' #есть пароль с которым мы должны свериться
+user_password = '123qwerty456'
 
-def input_password():
+in_password = ""
 
-    while in_password != user_password:
+while in_password != user_password:
+    in_password = str(input("Введите пароль: "))
+    if in_password != user_password:
         print("Пароль не верный!")
-        in_password = str(input("Введите пароль: "))
 
+if in_password == user_password:
+    print("Пароль принят!")
 
-    if in_password == user_password:
-        print("Пароль принят!")
