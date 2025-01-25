f5b150f403f6a0fe078480744e5ddb531e454d3e
Solovey1408 <songfromredstone@gmail.com>
2025-01-25 12:27:16 +0300

    ДЗ: Количество уникальных элементов

---
+++ HOMEWORK Try #2/HW №28 Number of unique items.py
@@ -0,0 +1,10 @@
+# Напишите программу, в которой создадите и заполните список,
+# а затем посчитаете количество уникальных элементов в нём и выведите на экран.
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(my_list)
+
+unique_elements = set(my_list)
+print(unique_elements)
+
+number_unique_elements = len(unique_elements)
+print(number_unique_elements)
