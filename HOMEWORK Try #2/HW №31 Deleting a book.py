0809242deea970c41bdf677c594bcbc2db77d55f
Solovey1408 <songfromredstone@gmail.com>
2025-01-27 18:18:34 +0300

    ДЗ: Удаление книги

--- HOMEWORK Try #2/HW №31 Deleting a book.py
+++ HOMEWORK Try #2/HW №31 Deleting a book.py
@@ -1,5 +1,26 @@
 # Реализуйте функцию `remove_book(title)`, которая удаляет книгу из словаря.
 # Если книга не найдена, программа должна вывести сообщение об этом.
+def add_book_to_library(title, author, year):
+    my_library[title] = {
+        "Author": author,
+        "Year": year,
+        "Availability": None  # При добавлении новой книги статус наличия None
+    }
+
+
+def add_book(title, author, year):
+    if title in my_library:
+        update_question = input("Такая книга уже есть.Обновить информацию о данной книге?").lower()
+        if update_question == "да":
+            add_book_to_library(title, author, year)
+            print(f"Информация книги '{title}' обновлена.")
+        else:
+            print(f"Информация книги '{title}' не обновлена.")
+    else:
+        add_book_to_library(title, author, year)
+        print(f"Книга '{title}' добавлена.")
+
+
 def remove_book_to_library(title):
     my_library[title] = {
         "Author": author,
@@ -8,22 +29,22 @@ def remove_book_to_library(title):
     }
 
 
-#Удаление методом del
-# def remove_book(title):
-#     if title not in my_library:
-#         print(f"Книга '{title}' отсутствует в библиотеке.")
-#     else:
-#         del my_library[title]
-#         print(f"Книга '{title}' удалена.")
-
-
-#Удаление методом pop
+# Удаление методом del
 def remove_book(title):
     if title not in my_library:
         print(f"Книга '{title}' отсутствует в библиотеке.")
     else:
-        delete_library = my_library.pop(title)
+        del my_library[title]
         print(f"Книга '{title}' удалена.")
+
+
+# #Удаление методом pop
+# def remove_book(title):
+#     if title not in my_library:
+#         print(f"Книга '{title}' отсутствует в библиотеке.")
+#     else:
+#         delete_library = my_library.pop(title)
+#         print(f"Книга '{title}' удалена.")
 
 
 my_library = {
@@ -49,6 +70,9 @@ my_library = {
     }
 }
 
+
+add_book("The Beach", "Alex Garland", 1995)
+add_book("Alice’s Adventures in Wonderland", "Lewis Carroll", 1865)
 
 remove_book("The Beach")
 remove_book("The Beach")
