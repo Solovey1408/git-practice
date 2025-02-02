d39fff4738e83eb6d792cdcc270f1708f2427507
Solovey1408 <songfromredstone@gmail.com>
2025-02-02 14:49:38 +0300

    ДЗ: Работа с классами и методами классов

--- HOMEWORK Try #2/main.py
+++ HOMEWORK Try #2/main.py
@@ -1,47 +1,60 @@
 class ToDoList:
 
-
     def __init__(self):
         self.tasks = []
 
+    def check_task(self, name):
 
-    def add_task(self, task):
-        self.tasks.append({"task": task, "complete": False})
-        print(f"Задача '{task}' добавлена.")
+        for i in self.tasks:
+            if i["name"] == name:
+                return True
+        return False
 
+    def add_task(self, name):
+        self.tasks.append({"name": name, "complete": False})
+        print(f"Задача '{name}' добавлена.")
 
-    def complete_task(self, task):
+    def complete_task(self, name):
+
+        if not self.tasks:  # Проверяем, есть ли задачи в списке
+            print("Задачи отсутствуют.")
+            return
 
         for i in self.tasks:
 
-            if i["task"] == task:
+            if i["name"] == name:
                 i["complete"] = True
-                print(f"Задача '{task}' выполнена")
+                print(f"Задача '{name}' выполнена")
                 break
-            else:
-                print(f"Задача '{task}' не найдена")
+        else:
+            print(f"Задача '{name}' не найдена")
 
+    def remove_task(self, name):
 
-    def remove_task(self, task):
+        if not self.tasks:  # Проверяем, есть ли задачи в списке
+            print("Список задач пуст.")
+            return
 
         for i in self.tasks:
 
-            if i["task"] == task:
+            if i["name"] == name:
                 self.tasks.remove(i)
-                print(f"Задача '{task}' удалена")
+                print(f"Задача '{name}' удалена")
                 break
-            else:
-                print(f"Задача '{task}' не найдена")
-
+        else:
+            print(f"Задача '{name}' не найдена")
 
     def list_tasks(self):
-         if not self.tasks:
-             print("Задач нет.")
-         else:
-             print("Список задач:")
-             for i in self.tasks:
-                 sign = "✔" if i["complete"] else "❌"
-                 print(f"{sign} {i}")
+
+        if not self.tasks:
+            print("Задач нет.")
+
+        else:
+            print("Список задач:")
+
+        for i in self.tasks:
+            sign = "✔" if i["complete"] else "❌"
+            print(f"{sign} {i["name"]}")
 
 
 my_todo_list = ToDoList()
@@ -50,8 +63,6 @@ my_todo_list = ToDoList()
 my_todo_list.add_task("Добавить")
 my_todo_list.add_task("Выполнить")
 my_todo_list.add_task("Удалить")
-my_todo_list.list_tasks()
 my_todo_list.complete_task("Добавить")
-my_todo_list.list_tasks()
 my_todo_list.remove_task("Добавить")
 my_todo_list.list_tasks()
